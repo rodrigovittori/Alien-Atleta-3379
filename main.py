84cf10bdab4b10e9607831da86cd6c6e031ca8d8
Rodrigo Vittori <vittorirodrigo@gmail.com>
2024-12-09 21:22:44 -0300

    [M5.L4 - Actividad #3: "Enemigos aleatorios"]
    Modificamos el sistema de avance de los enemigos para enfrentar un único obstáculo/enemigo cada vez

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 #pgzero
+import random
 
 """
-Nota: La Actividad #1 de esta clase (M5.L4) NO está relacionada a este juego.
+# [M5.L4 - Actividad #3: "Enemigos aleatorios"]
+# Objetivo: Llamar al próximo obstáculo/enemigo según un valor random
 
-# [M5.L4 - Actividad #2: "Funciones de movimiento"]
-# Objetivo: Migrar la lógica de movimiento de los enemigos a una función avocada a ello
+Modificamos el sistema de obstáculos para actualizar uno a la vez, que se determina de forma aleatoria
 
-1º Crearemos las funciones actualizar_abeja() y actualizar_caja()
+Pasos:
+1º Importar el módulo random
+2º Crear la vble "prox_enemigo", que tomará un valor random entre 1 y 2
+   (xq sólo tenemos dos tipos de enemigos)
+3º Creamos una función que calcule el valor de nuestro próximo enemigo en forma aleatoria
+4º En update(dt) llamaremos a la función que actualiza el obstáculo/enemigo "activo",
+    hasta que salga de la pantalla, (luego reasignaremos ese valor random)
+5º Agregar a la función reiniciar_juego una condición para reestablecer el valor del prox_enemigo a spwanear
 
-Nota 2: Como volvemos a tener el sprite "hurt", reactivamos el código
-
-Nota 3: Creamos también las funciones: mover_personaje(), detectar_colisiones(), reiniciar_juego()
-
-Nota 4: Increíblemente si inicalizamos texto_colision con un valor vacío, nos devuelve el siguielte error:
-       > ExternalError:
-            InvalidStateError:
-                Failed to execute 'drawImage' on 'CanvasRenderingContext2D':
-                    The image argument is a canvas element with a width or height of 0.
-
-        para evitar dibujar un bloque de texto negro, vamos a remover el atributo 'background' del text.draw()
+Nota: Para evitar ver la caja cuando no se está moviendo modificamos su posición al inicializarla, resetearla y moverla
 
 """
 
@@ -52,17 +50,18 @@ personaje.esta_agachado = False
 
 fondo = Actor("background")
 cartel_game_over = Actor("GO")
-caja  = Actor("box", (WIDTH-50, 265)) 
+caja  = Actor("box", (WIDTH + 50, 265)) 
 abeja = Actor("bee", (WIDTH + 350, 150))
 
 # Variables
 anim = 1 # variable temporal para demostrar las animaciones
 COOLDOWN_SALTO = 0.6 # tiempo de recarga habilidad salto (en segundos)
 timer_salto = 0 # tiempo que debe pasar (en segundos) antes de que nuestro personaje pueda saltar nuevamente
-nva_imagen = "alien" # "alien": quieto / "left": mov. izq. / "right" : mov. dcha.
+nva_imagen = "alien" # "alien": quieto / "left": mov. izq. / "right" : mov. dcha. / "hurt": tomó daño
 game_over = False
 texto_colision = " "
 puntuacion = 0 # Cantidad de enemigos esquivados
+prox_enemigo = random.randint(1,2) # 1: Caja / 2: Abeja
 
 """  #####################
     # FUNCIONES PROPIAS #
@@ -78,8 +77,17 @@ def animar(op):
     else:
         animate(personaje, tween="bounce_start_end", duration = 2, x= 35, y = HEIGHT-45)
 
+def enemigo_al_azar():
+    """
+    Numero entero de 1 a (cant_enemigos)
+    1: Caja
+    2: Abeja
+    """
+    return random.randint(1, 2) # Nota: actualizar cuando se agreguen tipos de enemigos
+
+
 def actualizar_abeja():
-    global puntuacion
+    global puntuacion, prox_enemigo
 
     # To-Do: agregar "zig-zagueo" de la abeja
     
@@ -87,13 +95,15 @@ def actualizar_abeja():
     if (abeja.x < (-int(abeja.width))):
         puntuacion += 1
         abeja.x = WIDTH + 150
+        prox_enemigo = enemigo_al_azar()
+        
     else:
         abeja.x -= 5 # mover la caja 5 px a la izquierda en cada frame
         # Nota: Posibilidad de que los enemigos aceleren cada vez que respawnean
         #       -> x.- vble global: velocidad_enemigos
 
 def actualizar_caja():
-    global puntuacion
+    global puntuacion, prox_enemigo
 
     # To-Do: agregar "zig-zagueo" de la abeja
     
@@ -106,7 +116,8 @@ def actualizar_caja():
     # Posición
     if (caja.x < (int(caja.width/2))):
         puntuacion += 1
-        caja.x += WIDTH
+        caja.x = WIDTH + 50
+        prox_enemigo = enemigo_al_azar()
     else:
         caja.x -= 5 # mover la caja 5 px a la izquierda en cada frame
         # Nota: Posibilidad de que los enemigos aceleren cada vez que respawnean
@@ -158,7 +169,7 @@ def mover_personaje():
         personaje.esta_agachado = True
 
 def reiniciar_juego():
-    global game_over, puntuacion, nva_imagen, timer_salto, texto_colision
+    global game_over, puntuacion, nva_imagen, timer_salto, texto_colision, prox_enemigo
     
     game_over = False
     puntuacion = 0
@@ -171,6 +182,7 @@ def reiniciar_juego():
     caja.angle = 0
     abeja.pos = (WIDTH + 350, 150)
     texto_colision = " "
+    prox_enemigo = enemigo_al_azar()
 
 """  ####################
     # FUNCIONES PGZERO #
@@ -225,11 +237,19 @@ def update(dt): # Podemos traducir "update" como "actualizar", es decir, en ella
         if ((personaje.timer_agachado <= 0) and (personaje.esta_agachado)): 
             personaje.y = 240
             personaje.esta_agachado = False
+
+        
         
         ########################
-        actualizar_caja()
-        actualizar_abeja()
-    
+
+        # Actualizamos los enemigos
+        # Nota: creo que ésto después lo migramos a otra función
+        if (prox_enemigo == 1):
+            actualizar_caja()
+        elif (prox_enemigo == 2):
+            actualizar_abeja()
+            # Nota: actualizar cuando se agreguen tipos de enemigos
+   
         mover_personaje()
     
         detectar_colisiones()
